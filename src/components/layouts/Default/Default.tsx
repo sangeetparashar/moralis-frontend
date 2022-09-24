9d111fcd0552ca9ebf682ede325c9fe59fcb810d
Sangeet Parashar <saparashar@microsoft.com>
2022-09-24 11:44:25 -0700

    remove header

--- src/components/layouts/Default/Default.tsx
+++ src/components/layouts/Default/Default.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from 'react';
 import { Container } from '@chakra-ui/react';
-import { Footer, Header } from 'components/modules';
+import { Footer } from 'components/modules';
 import Head from 'next/head';
 
 const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, pageName }) => (
@@ -9,7 +9,6 @@ const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, page
       <title>{`${pageName} | ETH Boilerplate`}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <Header />
     <Container maxW="container.lg" p={3} marginTop={100} as="main" minH="70vh" backgroundColor={"rgb(45,44,44"}>
       {children}
     </Container>
